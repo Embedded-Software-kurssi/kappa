c33f469effbdf97ee28ca494c4305493d42fc67d
jaska <emt@tu.fi>
2017-01-12 12:22:09 +0200

    Kapaaa

---
+++ kappa/kappa.ino
@@ -0,0 +1,99 @@
+
+
+ #define BIT8 128
+ #define BIT7 64
+ #define BIT6 32
+ #define BIT5 16
+ #define BIT4 8
+ #define BIT3 4
+ #define BIT2 2
+ #define BIT1 1
+
+
+ unsigned char numbers[10] = {
+   254, // 0
+   176, // 1
+   237, // 2
+   249, // 3
+   179, // 4
+   219, // 5
+   159, // 6
+   240, // 7
+   255, // 8
+   243, // 9
+ };
+ 
+int joyStickX = A0;
+int joyStickY = A1;
+int joyStickZ = 3;
+int motorPin = 11;
+
+int xZero = 517;
+int xRatio = 517/256;
+int yZero = 508;
+
+void setup() {
+  pinMode (joyStickX, INPUT);
+  pinMode (joyStickY, INPUT);
+  pinMode (joyStickZ, INPUT_PULLUP);
+  pinMode(motorPin, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  Serial.begin (9600); // 9600 bps
+}
+
+void loop() {
+  int x, y, z;
+  x = analogRead(joyStickX);
+  y = analogRead(joyStickY);
+  z = digitalRead(joyStickZ);
+  int speed = (abs(x-xZero)+4)/xRatio;
+  if (speed > 255) speed = 255;
+  float s = speed*9/255;
+  Serial.println(s);
+
+  showNumber(s);
+  analogWrite(motorPin, speed);
+
+  Serial.print (speed, DEC);
+  Serial.print (",");
+  Serial.print (abs(y-yZero), DEC);
+  Serial.print (",");
+  Serial.println (z, DEC);
+
+  delay(100);
+}
+
+void showNumber(char c) {
+     bitAction(BIT7 & numbers[c], 4);
+     bitAction(BIT6 & numbers[c], 5);
+     bitAction(BIT5 & numbers[c], 6);
+     bitAction(BIT4 & numbers[c], 7);
+     bitAction(BIT3 & numbers[c], 8);
+     bitAction(BIT2 & numbers[c], 9);
+     bitAction(BIT1 & numbers[c], 10);
+}
+
+ void bitAction(char b, char pin) {
+  Serial.println("action");
+   if (!b) {
+     digitalWrite(pin, HIGH);
+   }
+   else {
+     digitalWrite(pin, LOW);
+   }
+ }
